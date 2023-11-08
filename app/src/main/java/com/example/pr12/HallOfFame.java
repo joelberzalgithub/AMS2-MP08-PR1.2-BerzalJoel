df6e434e76182440d74be4f5b1c1b990ad9844e9
joelberzalgithub <120808626+joelberzalgithub@users.noreply.github.com>
2023-11-08 10:55:16 +0100

    Segon commit del joc d'endevinar el número

---
+++ app/src/main/java/com/example/pr12/HallOfFame.java
@@ -0,0 +1,42 @@
+package com.example.pr12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.EditText;
+public class HallOfFame extends AppCompatActivity {
+    /*
+    private final int receivedValue = getIntent().getIntExtra("intents", 0);
+    public void showNewRecordDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Hall of fame");
+        builder.setMessage("Abans d'anar a la taula de rècords, t'agradaria afegir-hi el teu propi rècord?");
+
+        EditText input = new EditText(this);
+        builder.setView(input);
+
+        builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // String value = String.valueOf(input.getText());
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hall_of_fame);
+
+        showNewRecordDialog();
+    }
+    */
+}
--- app/src/main/java/com/example/pr12/MainActivity.java
+++ app/src/main/java/com/example/pr12/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pr12;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.KeyEvent;
@@ -16,20 +17,28 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
+    private Intent intent;
     private void showGameOverDialog() {
+
+        intent = new Intent(MainActivity.this, HallOfFame.class);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Fi de la partida");
         builder.setMessage("Has endevinat el número. Felicitats!");
 
         builder.setPositiveButton("Tornar a jugar", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("Mostrar taula de rècords", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                startActivity(intent);
                 dialog.dismiss();
             }
         });
         builder.create().show();
     }
-
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
@@ -60,29 +69,29 @@ public class MainActivity extends AppCompatActivity {
 
         button.setOnClickListener(new View.OnClickListener() {
             int randomNumber = (int) ((Math.random() * (100 - 1 + 1)) + 1);
-            Toast toast;
+            int numIntents = 0;
 
             public void onClick(View v) {
 
                 if (randomNumber > Integer.parseInt(String.valueOf(editText.getText()))) {
-                    toast = Toast.makeText(getApplicationContext(), "El nombre que busques és major que " + editText.getText(), Toast.LENGTH_SHORT);
-                    toast.show();
-                    textView.append(editText.getText() + "  ");
+                    Toast.makeText(getApplicationContext(), "El nombre que busques és major que " + editText.getText(), Toast.LENGTH_SHORT).show();
+                    textView.append(editText.getText() + "\n");
                 }
 
                 else if (randomNumber < Integer.parseInt(String.valueOf(editText.getText()))) {
-                    toast = Toast.makeText(getApplicationContext(), "El nombre que busques és menor que " + editText.getText(), Toast.LENGTH_SHORT);
-                    toast.show();
-                    textView.append(editText.getText() + "  ");
+                    Toast.makeText(getApplicationContext(), "El nombre que busques és menor que " + editText.getText(), Toast.LENGTH_SHORT).show();
+                    textView.append(editText.getText() + "\n");
                 }
 
                 else {
                     showGameOverDialog();
                     textView.setText("");
                     randomNumber = (int) ((Math.random() * (100 - 1 + 1)) + 1);
+                    intent.putExtra("intents", numIntents);
                 }
                 editText.setText("");
                 button.setEnabled(false);
+                numIntents++;
             }
         });
     }
